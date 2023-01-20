cdeace55db1fc505a0c1fdcd4f4c829627394c2c
Mylena Vitoriano <mylenabarbosa08@gmail.com>
2023-01-20 17:17:13 -0300

    finalizado ajustes pequenos no layout

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
 
 const data = {
-    run: []
+    yoga: ['01-17', '01-18', '01-20'],
+    water: ['01-17', '01-19', '01-20'],
+    study: ['01-10', '01-18', '01-19'],
+    sleep: ['01-15', '01-17', '01-16'],
 }
+
+nlwSetup.setData(data)
+nlwSetup.load()
