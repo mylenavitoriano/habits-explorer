788e2c4375f051d83c511e88eb17e2ebb36e4fb1
Mylena Vitoriano <mylenabarbosa08@gmail.com>
2023-01-23 15:32:03 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-    yoga: ['01-17', '01-18', '01-20'],
-    water: ['01-17', '01-19', '01-20'],
-    study: ['01-10', '01-18', '01-19'],
-    sleep: ['01-15', '01-17', '01-16'],
+button.addEventListener('click', add)
+form.addEventListener("change", save)
+
+function add (){
+    const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if(dayExists){
+        alert("Dia já incluso")
+        return
+    }
+
+    nlwSetup.addDay(today)
 }
 
+function save(){
+    localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
